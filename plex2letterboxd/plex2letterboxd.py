0a126474a411b2dc5957c3c7d95972714098ff89
Arjun Earthperson <mail@earthperson.org>
2023-10-14 23:34:11 -0400

    intial commit

--- plex2letterboxd/plex2letterboxd.py
+++ plex2letterboxd/plex2letterboxd.py
@@ -4,69 +4,120 @@ import configparser
 import csv
 import sys
 
+from plexapi.myplex import MyPlexUser, MyPlexAccount
 from plexapi.server import PlexServer
+from typing import List, Any
+import concurrent.futures
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Export watched Plex movies to the Letterboxd import '
-                    'format.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--ini', default='config.ini',
-                        help='config file')
-    parser.add_argument('-o', '--output', default='letterboxd.csv',
-                        help='file to output to')
-    parser.add_argument('-s', '--sections', default=['Movies'], nargs='+',
-                        help='sections to grab from')
-    parser.add_argument('-m', '--managed-user',
-                        help='name of managed user to export')
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command line arguments.
+
+    Returns:
+        argparse.Namespace: Namespace object with all arguments.
+    """
+    about: str = 'Export watched Plex movies to the Letterboxd import format.'
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description=about, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--ini', default='config.ini', help='config file')
+    parser.add_argument('-o', '--output', default='letterboxd.csv', help='file to output to')
+    parser.add_argument('-s', '--sections', default=['Movies'], nargs='+', help='sections to grab from')
+    parser.add_argument('-m', '--managed-user', help='name of managed user to export')
     return parser.parse_args()
 
 
-def parse_config(ini):
-    """Read and validate config file."""
-    config = configparser.ConfigParser()
+def parse_config(ini: str) -> configparser.SectionProxy:
+    """
+    Parse configuration file.
+
+    Args:
+        ini (str): Path to the configuration file.
+
+    Returns:
+        configparser.SectionProxy: SectionProxy object with configuration values.
+
+    Raises:
+        SystemExit: If any required configuration values are missing.
+    """
+    config: configparser.ConfigParser = configparser.ConfigParser()
     config.read(ini)
-    auth = config['auth']
-    missing = {'baseurl', 'token'} - set(auth.keys())
+    auth: configparser.SectionProxy = config['auth']
+    missing: set = {'baseurl', 'token'} - set(auth.keys())
     if missing:
         print(f'Missing the following config values: {missing}')
         sys.exit(1)
     return auth
 
 
-def write_csv(sections, output):
-    """Generate Letterboxd import CSV."""
+def fetch_movie_details(movie):
+    """
+    Fetch details of a movie.
+
+    Args:
+        movie: Movie object.
+
+    Returns:
+        list: List containing movie title, year, rating and date.
+    """
+    date: str = None
+    if movie.lastViewedAt is not None:
+        date = movie.lastViewedAt.strftime('%Y-%m-%d')
+    rating: str = movie.userRating
+    if rating is not None:
+        rating = f'{movie.userRating:.0f}'
+    print(movie.title, movie.year, rating, date)
+    return [movie.title, movie.year, rating, date]
+
+
+def fetch_section_details(sections, max_workers=None):
+    """
+       Fetch details of all movies in the given sections.
+
+       Args:
+           sections (list): List of section objects.
+           max_workers (int, optional): Maximum number of threads that can be used to execute the given calls.
+
+       Returns:
+           list: List of lists containing details of all movies.
+    """
+    all_movie_details = []
+    for section in sections:
+        found_movies = section.search(sort='lastViewedAt', unwatched=False)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            movie_details = list(executor.map(fetch_movie_details, found_movies))
+            all_movie_details.extend(movie_details)
+    return all_movie_details
+
+
+def write_csv(sections: List[Any], output: str) -> None:
+    """
+    Write movie details to a CSV file.
+
+    Args:
+        sections (List[Any]): List of section objects.
+        output (str): Path to the output file.
+    """
     with open(output, 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['Title', 'Year', 'Rating10', 'WatchedDate'])
-
-        count = 0
-        for section in sections:
-            for movie in section.search(sort='lastViewedAt', unwatched=False):
-                date = None
-                if movie.lastViewedAt is not None:
-                    date = movie.lastViewedAt.strftime('%Y-%m-%d')
-                rating = movie.userRating
-                if rating is not None:
-                    rating = f'{movie.userRating:.0f}'
-                writer.writerow([movie.title, movie.year, rating, date])
-                count += 1
-    print(f'Exported {count} movies to {output}.')
+        all_movie_details = [['Title', 'Year', 'Rating10', 'WatchedDate']]
+        all_movie_details.extend(fetch_section_details(sections))
+        writer.writerows(all_movie_details)
 
 
-def main():
-    args = parse_args()
-    auth = parse_config(args.ini)
+def main() -> None:
+    """
+    Main function to parse arguments, parse configuration, fetch movie details and write them to a CSV file.
+    """
+    args: argparse.Namespace = parse_args()
+    auth: configparser.SectionProxy = parse_config(args.ini)
 
-    plex = PlexServer(auth['baseurl'], auth['token'])
+    plex: PlexServer = PlexServer(auth['baseurl'], auth['token'])
     if args.managed_user:
-        myplex = plex.myPlexAccount()
-        user = myplex.user(args.managed_user)
-        # Get the token for your machine.
-        token = user.get_token(plex.machineIdentifier)
-        # Login to your server using your friends credentials.
+        myplex: MyPlexAccount = plex.myPlexAccount()
+        user: MyPlexUser = myplex.user(args.managed_user)
+        token: str = user.get_token(plex.machineIdentifier)
         plex = PlexServer(auth['baseurl'], token)
 
-    sections = [plex.library.section(s) for s in args.sections]
+    sections: List[Any] = [plex.library.section(s) for s in args.sections]
     write_csv(sections, args.output)
+    
